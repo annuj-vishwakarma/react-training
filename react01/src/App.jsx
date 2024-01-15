1f767aeceb7688f8ace8a1046989621d0bb6f341
anuj <vishwakarmaanuj61@gmail.com>
2024-01-15 20:09:41 +0530

    2nd

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   return (
    <div>
-    <h1>hello Anuj</h1>
+    <h1>hello Anuj its time to win that day</h1>
     </div>
   )
 }
 
-export default App
+export default App 

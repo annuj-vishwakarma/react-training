c5c4cbe1799e1eaa852e9abf5da9ad9648da4391
anuj <vishwakarmaanuj61@gmail.com>
2024-01-17 23:52:44 +0530

    restart

--- react01/src/App.jsx
+++ react01/src/App.jsx
@@ -5,21 +5,7 @@ const App = () => {
   const username = "Anuj"
   return (
    <div className='full-size h-screen bg-black'>
-    <nav className='full-size h-12 bg-white flex justify-between pl-10 text-xl items-center font-medium'>
-<div>
-  Anuj
-</div>
-<div className='flex gap-4 pr-10'>
-  <h1>contact</h1>
-  <h1>About</h1>
-  <h1>Blog</h1>
-</div>
-    </nav>
-    <h1 className='text-white'>hello {username} its time to win that day!!!</h1>
-    <h1 className='text-white'>Currently i am learning react</h1>
-    <div>
-      <button className='text-white border '>contact me</button>
-    </div>
+  
     </div>
 
 

ecb4f4b2b2c9c51c3723fb497f50e36603e585d0
anuj <vishwakarmaanuj61@gmail.com>
2024-01-18 00:31:33 +0530

    updated

--- 01costom/src/App.jsx
+++ 01costom/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const App = () => {
+
+let counter = 15;
+
+const addValue=()=>{
+  console.log("clicked", counter);
+  counter = counter + 1
+}
+
   return (
-    <h1>counter Value : 5</h1>
+   <div>
+     <h1>counter Value :{counter} </h1>
+     <button
+     onClick={addValue}
+     
+     >Add Value</button>
+     <br />
+     <br />
+     <button>Remove Value</button>
+   </div>
+    
   )
 }
 

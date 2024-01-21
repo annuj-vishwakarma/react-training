8516decb00a9fa97afc6a31a50b4133ee5dd1ced
anuj <vishwakarmaanuj61@gmail.com>
2024-01-21 17:34:22 +0530

    chay or code

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useCallback, useState  } from 'react'
 
 const App = () => {
+
+  const [length ,setLenght]=useState(8)
+  const [numberAllowed, setNumberAllowed]=useState(false)
+  const [charAllowed, setCharAllowed]=useState(false)
+  const [password,setPassword]=useState("")
+
+const passwordGenerator=useCallback(()=>{
+let pass =""
+let str ="ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+if(condition){
+  
+}
+
+
+})[length,numberAllowed,charAllowed,setPassword]
+
   return (
-    <div className='bg-black w-full h-screen text-white'>App</div>
+    <>
+<h1 className='text-4xl text-center text-white'>Password Generator</h1>
+
+    </>
   )
 }
 

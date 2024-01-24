1f0f321aa66f281417e39e8054375cba1e5840ea
anuj <vishwakarmaanuj61@gmail.com>
2024-01-24 23:10:06 +0530

    07reactRouter

--- 07reactRouter/src/components/Contact/Contact.jsx
+++ 07reactRouter/src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                            minal street bhopal
+                            Minal Street Bhopal
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 93406204**
                                 </div>
                             </div>
 
